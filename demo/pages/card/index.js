6a261009c14e5015e2988f178cff72b7ae64ac03
zhongwu <xingya.sxy@alibaba-inc.com>
2021-09-06 23:30:27 +0800

    feat:Init

---
+++ demo/pages/card/index.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    thumb: 'https://gw.alipayobjects.com/mdn/rms_ce4c6f/afts/img/A*b-kqQ4RZgsYAAAAAAAAAAABkARQnAQ',
+    expand3rd: false,
+  },
+  onCardClick(ev) {
+    my.alert({
+      content: ev.info,
+    });
+  },
+  onActionClick() {
+    my.alert({
+      content: 'action clicked',
+    });
+  },
+  onExtraActionClick() {
+    my.alert({
+      content: 'extra action clicked',
+    });
+  },
+  toggle() {
+    this.setData({
+      expand3rd: !this.data.expand3rd,
+    });
+  },
+});
